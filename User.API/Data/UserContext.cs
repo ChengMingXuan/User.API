27abf1ad619a484a3f7f9f52556d51e638778806
小萝卜儿头 <18810114757@163.com>
2018-06-04 18:49:27 +0800

    添加项目文件。

---
+++ User.API/Data/UserContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using User.API.Models;
+
+namespace User.API.Data
+{
+    public class UserContext : DbContext
+    {
+        public   UserContext(DbContextOptions<UserContext> options):base(options){
+
+         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //指定数据库表名称 Users
+            modelBuilder.Entity<AppUser>().ToTable("Users").HasKey(u => u.Id);
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ User.API/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using User.API.Data;
+
+namespace User.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<UserContext>(option=> {
+                option.UseMySQL(Configuration.GetConnectionString("MysqlUser"));
+            });
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            InitUserDatabase(app);
+        }
+
+        public void InitUserDatabase(IApplicationBuilder app)
+        {
+            using (var scope=app.ApplicationServices.CreateScope()) {
+                var userContext = scope.ServiceProvider.GetRequiredService<UserContext>();
+                userContext.Database.Migrate();
+                if (!userContext.Users.Any())
+                {
+                    userContext.Users.Add(new Models.AppUser { Name="jgsy"});
+                    userContext.SaveChanges();
+                }
+
+            }
+        }
+    }
+}
